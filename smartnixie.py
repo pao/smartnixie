f7c1c6a5d21603509f319c2e6658aba6802e41aa
Patrick O'Leary <patrick.oleary@gmail.com>
2019-10-02 23:12:33 -0500

    add cycle mode to run during early morning
    For an hour each morning, cycle through all of the display elements
    to help protect against cathode poisoning instead of showing the
    current time.

--- smartnixie.py
+++ smartnixie.py
@@ -18,6 +18,8 @@ def setdigits(addrs, vals, sep):
     chars = list(divmod(vals, 10))
     if sep:
         chars[0] = chars[0] | 0x40
+    if sep > 1:
+        chars[1] = chars[1] | 0x40
     for addr, val in zip(addrs, chars):
         bus.write_byte_data(addr, REG_DIGIT, val)
 
@@ -31,7 +33,14 @@ tfd.timerfd_settime(timer, tv, tfd.TFD_TIMER_ABSTIME)
 while True:
     ep.poll()
     t = time.localtime()
-    setdigits(hh, t.tm_hour, False)
-    setdigits(mm, t.tm_min, t.tm_sec%2)
-    setdigits(ss, t.tm_sec, t.tm_sec%2)
+    if 3 <= t.tm_hour < 4:
+        digit = (t.tm_min*60+t.tm_sec)%10
+        digits = 10*digit+digit
+        setdigits(hh, digits, t.tm_sec%2*2)
+        setdigits(mm, digits, t.tm_sec%2*2)
+        setdigits(ss, digits, t.tm_sec%2*2)
+    else:
+        setdigits(hh, t.tm_hour, False)
+        setdigits(mm, t.tm_min, t.tm_sec%2)
+        setdigits(ss, t.tm_sec, t.tm_sec%2)
     read(timer, 8)
